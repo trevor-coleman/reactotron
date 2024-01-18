07019540d93d318afddc085a7c6f5e42ed3b23dc
Trevor Coleman <trevor@trevorcoleman.design>
2024-01-18 11:06:51 -0500

    docs(ci): add docs for ci:trust command

--- scripts/git-push-fork-to-upstream-repo.sh
+++ scripts/git-push-fork-to-upstream-repo.sh
@@ -17,19 +17,19 @@ SOURCE_GH_USER=$(echo "$BRANCH_SPEC" | awk -F: '{print $1}')
 SOURCE_BRANCH=$(echo "$BRANCH_SPEC" | awk -F: '{print $2}')
 REPO_NAME=$(git remote get-url --push origin | awk -F/ '{print $NF}' | sed 's/\.git$//')
 
-# Check if 'fork-to-test' remote exists and then remove it
-if git config --get "remote.fork-to-test.url" > /dev/null; then
-    git remote remove fork-to-test
-    echo "Removed remote fork-to-test"
+# Check if 'temp-branch-to-test-fork' remote exists and then remove it
+if git config --get "remote.temp-branch-to-test-fork.url" > /dev/null; then
+    git remote remove temp-branch-to-test-fork
+    echo "Removed remote temp-branch-to-test-fork"
 else
-    echo "Remote fork-to-test does not exist, no need to remove it"
+    echo "Remote temp-branch-to-test-fork does not exist, no need to remove it"
 fi
 
-git remote add fork-to-test "git@github.com:$SOURCE_GH_USER/$REPO_NAME.git"
+git remote add temp-branch-to-test-fork "git@github.com:$SOURCE_GH_USER/$REPO_NAME.git"
 
 git fetch --all
-git push --force "$REACTOTRON_REPO" "refs/remotes/fork-to-test/$SOURCE_BRANCH:refs/heads/$GPF_REACTOTRON_BRANCH"
-git remote remove fork-to-test || echo "Removed new remote fork-to-test"
+git push --force "$REACTOTRON_REPO" "refs/remotes/temp-branch-to-test-fork/$SOURCE_BRANCH:refs/heads/$GPF_REACTOTRON_BRANCH"
+git remote remove temp-branch-to-test-fork || echo "Removed new remote temp-branch-to-test-fork"
 
 cat <<EOF
 Forked branch '$BRANCH_SPEC' has been pushed to branch '$GPF_REACTOTRON_BRANCH'
